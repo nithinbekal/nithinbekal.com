6ccb22c31c6caa6a00a8f309e303398cbfe24dae
Nithin Bekal <nithinbekal@gmail.com>
2012-06-22 22:33:19 +0530

    Clean up single page template

--- page.php
+++ page.php
@@ -1,73 +1,48 @@
 <?php
 /**
  * @package WordPress
- * @subpackage MyTheme
+ * @subpackage nithinbekal
  */
 
 get_header(); ?>
 
-<section id="posts">
 
+    <section id="posts">
     <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+      
+      <article <?php post_class(); ?> id=post-"<?php the_ID(); ?>">
+      
+        <header>
+          <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+        </header>
+        
+        <section class="post-content">
+          <?php the_content('...'); ?>
+          <?php wp_link_pages(); ?>
+        </section>
+        
+        <footer class="post-footer">
+          <section id="comments-template"><?php comments_template(); ?></section>
+        </footer>
+      
+      </article>
 
-        <?php while (have_posts()) : the_post(); ?>
-
-            <article <?php post_class(); ?> id=post-"<?php the_ID(); ?>">
-                <header>
-                    <section class="post-meta-top">
-                        <time><?php the_date(); ?></time><br/>
-                        Posted by <?php the_author_posts_link(); ?><br/>
-                        <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
-                    </section>
-                    <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-                </header>
-
-                <section class="post-content">
-                            <?php the_content('Read the rest of this entry...'); ?>
-                            <?php wp_link_pages(); ?>
-                </section>
-                <footer class="post-footer">
-                    <section class="tags-categories">
-                        <p><b>Categories:</b> <?php the_category(', ') ?></p>
-                        <p><?php the_tags('<b>Tags:</b> ', ', ', '<br />'); ?></a></p>
-                    </section>
-                    <section id="comments-template">
-                        <?php comments_template(); ?>
-                    </section>
-                </footer>
-            </article>
-
-        <?php endwhile; ?>
-
-        <nav class="prev-next-links">
-            <ul>
-                <li class="prev-link"><?php previous_post('&laquo; %', '', 'yes'); ?></li>
-                <li class="next-link"><?php next_post('% &raquo; ', '', 'yes'); ?></li>
-            </ul>
-        </nav>
+      <?php endwhile; ?>
 
     <?php else: ?>
 
-        <p>Uh-oh! We couldn't find any posts matching that description in our database.
-            Why do't you try searching for it?
-        </p>
+      <p>Uh-oh! I couldn't find any pages matching that description in the database. Why don't you try searching for it?</p>
 
         <section id="search-404">
-            <form id="searchform" method="get" action="<?php bloginfo('home'); ?>">
-                <input type="text" name="s" id="s-404" value="" />
-                <input type="submit" id="submit-404" value="Search" />
-            </form>
-        </section>
-
-        <p>Or, you might want to read some of our most commented articles:</p>
-
-        <section class="popular-posts">
-            <ul><?php echo popularPosts(5); ?></ul>
+          <form id="searchform-404" method="get" action="<?php bloginfo('home'); ?>">
+            <input type="text" name="s" id="s-404" value="" />
+            <input type="submit" id="submit" value="Search" />
+          </form>
         </section>
 
     <?php endif; ?>
 
 </section>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @subpackage wp_monochrome
+ * @subpackage nithinbekal
  */
 
 get_header(); ?>
