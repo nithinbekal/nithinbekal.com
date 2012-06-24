a23dd09af562cf1bbcf48c3a094f216edb627aba
Nithin Bekal <nithinbekal@gmail.com>
2012-06-24 17:42:43 +0530

    Improve 404 styles

--- 404.php
+++ 404.php
@@ -7,15 +7,14 @@
 get_header(); ?>
 
 		<section id="posts">
-			<p>Uh-oh, the page you are looking for couldn't be found! Would you like to search for the content instead?</p>
+        <p>Uh-oh, the page you are looking for couldn't be found! Would you like to search for the content instead?</p>
 
-			<section id="search-404">
-				<form id="searchform-404" method="get" action="<?php bloginfo('home'); ?>">
-					<input type="text" name="s" id="s-404" value="" />
-					<input type="submit" id="submit" value="Search" />
-				</form>
-			</section>
-
-	</section>
+        <section id="search-404">
+          <form id="searchform-404" method="get" action="<?php bloginfo('home'); ?>">
+            <input type="text" name="s" id="s-404" value="" />
+            <input type="submit" id="submit" value="Search" />
+          </form>
+        </section>
+    </section>
 
 <?php get_footer(); ?>
