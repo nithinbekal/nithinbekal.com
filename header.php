2b2eb74576e1f918d4f5fb80a8f494bc0d943576
Nithin Bekal <nithinbekal@gmail.com>
2012-06-24 20:41:30 +0530

    More responsiveness changes

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
   <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
